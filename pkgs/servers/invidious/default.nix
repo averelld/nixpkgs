185fff424b1c184e6f13545dd670bd0aaf3cb5e2
Averell Dalton <averell+nixpkgs@rxd4.com>
2019-12-11 05:46:08 +0100

    Upgrade to latest version

--- pkgs/servers/invidious/default.nix
+++ pkgs/servers/invidious/default.nix
@@ -1,43 +1,17 @@
-{ lib, crystal, fetchgit, git, libxml2, sqlite, openssl_1_0_2, imagemagick7, writeText }:
+{ lib, crystal, fetchgit, git, libxml2, sqlite, librsvg, pkgconfig }:
 crystal.buildCrystalPackage rec {
   pname = "invidious";
-  version = "0.19.1";
+  version = "0.20.1";
 
   src = fetchgit {
     url = "https://github.com/omarroth/invidious.git";
-    rev = version;
-    sha256 = "1y062ngy0wi6aa4bnz68c6jniy6c6bmz6lcvh40gz7c85rwqm23g";
+    rev = "88a538e71bd9810a8b47524b2ddc865ec6326285";
+    sha256 = "1abljpyi4w36qkicznl6ai3y5xsf66qcnnk5vkin18ji7vzkh6ji";
     # Needed for extracting commit history, as well as for git version embedding
     deepClone = true;
   };
 
-  nativeBuildInputs = [ git ];
-
-  patches = [ (writeText "patch" ''
-    diff --git a/src/invidious.cr b/src/invidious.cr
-    index 315363d..d0cda68 100644
-    --- a/src/invidious.cr
-    +++ b/src/invidious.cr
-    @@ -5202,4 +5202,5 @@ add_context_storage_type(Preferences)
-     add_context_storage_type(User)
-
-     Kemal.config.logger = logger
-    +Kemal.config.port = config.external_port || 3000
-     Kemal.run
-    diff --git a/src/invidious/helpers/utils.cr b/src/invidious/helpers/utils.cr
-    index 69aae83..91e1c14 100644
-    --- a/src/invidious/helpers/utils.cr
-    +++ b/src/invidious/helpers/utils.cr
-    @@ -209,7 +209,7 @@ end
-
-     def make_host_url(config, kemal_config)
-       ssl = config.https_only || kemal_config.ssl
-    -  port = config.external_port || kemal_config.port
-    +  port = kemal_config.port
-
-       if ssl
-         scheme = "https://"
-  '') ];
+  nativeBuildInputs = [ git pkgconfig ];
 
   postPatch = ''
     # Patch the assets and locales paths to be absolute
@@ -50,19 +24,15 @@ crystal.buildCrystalPackage rec {
     sed -i config/migrate-scripts/* src/invidious/helpers/helpers.cr \
       -e 's|config/sql|${placeholder "out"}/share/invidious/config/sql|g'
 
-    # https://github.com/omarroth/invidious/pull/729
-    sed -i config/migrate-scripts/migrate-db-3646395.sh \
-      -e 's|psql invidious <|psql invidious kemal <|g'
-
-    # Needed for captcha to work. Note that imagemagick7_light wouldn't work
+    # Needed for captcha to work.
     substituteInPlace src/invidious/users.cr \
-      --replace 'Process.run(%(convert' 'Process.run(%(${imagemagick7}/bin/convert'
+      --replace 'Process.run(%(rsvg-convert' 'Process.run(%(${librsvg}/bin/rsvg-convert'
   '';
 
   shardsFile = ./shards.nix;
   crystalBinaries.invidious.src = "src/invidious.cr";
 
-  buildInputs = [ libxml2 sqlite openssl_1_0_2 ];
+  buildInputs = [ libxml2 sqlite ];
 
   postInstall = ''
     mkdir -p $out/nix-support/invidious $out/share/invidious/config
--- pkgs/servers/invidious/shards.nix
+++ pkgs/servers/invidious/shards.nix
@@ -1,15 +1,15 @@
 {
-  db = {
-    owner = "crystal-lang";
-    repo = "crystal-db";
-    rev = "dcd0af5ce81b5a43305e02846106633373f65004";
-    sha256 = "1gf6ylzcb8ywpy6m7w7d49xnvn6q636xb86f3vh292s1kml3vm99";
+  pg = {
+    owner = "will";
+    repo = "crystal-pg";
+    rev = "v0.19.0";
+    sha256 = "19w7saa30761jxflr7r3h93al7iidlp7mw39myd0kv65bwc4gv0v";
   };
-  exception_page = {
-    owner = "crystal-loot";
-    repo = "exception_page";
-    rev = "v0.1.2";
-    sha256 = "0j5ishhyriq9p339yaawrmawl9wgmp1paniq30a8d6a0568h3avq";
+  sqlite3 = {
+    owner = "crystal-lang";
+    repo = "crystal-sqlite3";
+    rev = "v0.14.0";
+    sha256 = "0hmfya6zj2nhyqk910hj250p5dhiw52s7pm3ykk9vlbfs4wj74mi";
   };
   kemal = {
     owner = "kemalcr";
@@ -17,28 +17,46 @@
     rev = "v0.26.0";
     sha256 = "0ja5q87djz43fdwc6wgvghgd0qly9pliwjcw4gidb8vwvjmn8hsx";
   };
+  pool = {
+    owner = "ysbaddaden";
+    repo = "pool";
+    rev = "v0.2.3";
+    sha256 = "00wxadhl6v82yyjl28kzmr9kv1yq6msv9dqhbsqdrdq0gg2hnvrs";
+  };
+  protodec = {
+    owner = "omarroth";
+    repo = "protodec";
+    rev = "v0.1.2";
+    sha256 = "1z7651ymjbvxrc6bgpxjbslm0agzd0pwvx1x8rbic2qdmzhfs21r";
+  };
+  lsquic = {
+    owner = "omarroth";
+    repo = "lsquic.cr";
+    rev = "v0.1.7";
+    sha256 = "0lmybnzah79p24wzxgg5iyspk4f0sd80wi3blc9rz2qjrbc0h3lp";
+  };
+  db = {
+    owner = "crystal-lang";
+    repo = "crystal-db";
+    rev = "v0.7.0";
+    sha256 = "0is08qrz5wc09j052jsj67zwmg362xv4k2wl353hn7lzim3s517j";
+  };
+  exception_page = {
+    owner = "crystal-loot";
+    repo = "exception_page";
+    rev = "v0.1.2";
+    sha256 = "0j5ishhyriq9p339yaawrmawl9wgmp1paniq30a8d6a0568h3avq";
+  };
   kilt = {
     owner = "jeromegn";
     repo = "kilt";
     rev = "v0.4.0";
     sha256 = "1w9ib6j4xhwxdxx58nzc06gw7ci7vga03vgj1z1bnklamb0b0l0k";
   };
-  pg = {
-    owner = "will";
-    repo = "crystal-pg";
-    rev = "v0.18.1";
-    sha256 = "0q2ckgwmyh03cd13lw0p2h7cyqdrggml39f3hw0hx2h4a7wjl9mg";
-  };
   radix = {
     owner = "luislavena";
     repo = "radix";
     rev = "v0.3.9";
     sha256 = "19pksfr7ddc31hvikb433jg0zav1ar93k6zmsgaf3vsrjnvia3ix";
-  };
-  sqlite3 = {
-    owner = "crystal-lang";
-    repo = "crystal-sqlite3";
-    rev = "v0.13.0";
-    sha256 = "0myd9vvz40wnwl5ij1n1sva63nnmdbfv6xka5fb4y7v77l512y7c";
   };
 }
